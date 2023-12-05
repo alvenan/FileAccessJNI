5efaadfe3d4e17b5d8aa1e4bca20b07fc87f4f7a
Alison Venancio <alison.venancio@gmail.com>
2023-12-05 13:11:03 -0400

    4. d) Declarando interfaces

--- app/src/main/java/vendor/alvenan/fileaccessjni/MainActivity.kt
+++ app/src/main/java/vendor/alvenan/fileaccessjni/MainActivity.kt
@@ -2,32 +2,42 @@ package vendor.alvenan.fileaccessjni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import vendor.alvenan.fileaccessjni.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-private lateinit var binding: ActivityMainBinding
+        private lateinit var textBox: TextView
+        private lateinit var filePath: EditText
+        private lateinit var message: EditText
+        private lateinit var writeBtn: Button
+        private lateinit var readBtn: Button
+        private lateinit var deleteBtn: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+            textBox = findViewById(R.id.sample_text)
+            filePath = findViewById(R.id.pathet)
+            message = findViewById(R.id.textet)
+            writeBtn = findViewById(R.id.writebtn)
+            readBtn = findViewById(R.id.readbtn)
+            deleteBtn = findViewById(R.id.deletebtn)
+        }
 
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        /**
+         * A native method that is implemented by the 'fileaccessjni' native library,
+         * which is packaged with this application.
+         */
+        external fun stringFromJNI(): String
+
+        companion object {
+            // Used to load the 'fileaccessjni' library on application startup.
+            init {
+                System.loadLibrary("fileaccessjni")
+            }
+        }
     }
-    /**
-      * A native method that is implemented by the 'fileaccessjni' native library,
-      * which is packaged with this application.
-      */
-     external fun stringFromJNI(): String
-
-     companion object {
-         // Used to load the 'fileaccessjni' library on application startup.
-         init {
-             System.loadLibrary("fileaccessjni")
-         }
-     }
-}
